a484b1f458fa0243f47918ce34161454e4695b7c
barzo <barzomiadam@gmail.com>
2024-11-17 21:06:03 +0100

    Añadido mi nombre a Readme Creado el enumerado con literales parametrizados creado el atributo String Constructor creado metodo to String creado creado un enumerado color con literales prametrizados creado el atributo cadenaAmostrar creado el constructor Color Creado el el metodo to String de la clase Color Creada la clase Posicón con sus atributos creados los respectivos getters y setters de los Atributos

--- src/test/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/test/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -1,6 +1,31 @@
 package org.iesalandalus.programacion.damas.modelo;
 
 public class Posicion {
-    int fila;
+    private int fila;
     private char columna;
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila>1&&fila<8) {
+            this.fila = fila;
+        }else{
+            throw new  IllegalArgumentException("Te has salido de las posiciones de la tabla");
+        }
+
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna >= 'A'&& columna<='H') {
+            this.columna = columna;
+        }else {
+            throw new IllegalArgumentException("Te has salido de las posiciones de la tanla");
+        }
+    }
 }
