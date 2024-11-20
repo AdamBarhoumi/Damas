2ece2c4c30c9dd123387dbc7e1acbbb3afafda54
barzo <barzomiadam@gmail.com>
2024-11-20 10:12:31 +0100

    Añadido mi nombre a Readme Creado el enumerado con literales parametrizados Creado el atributo String Constructor creado Metodo to String creado Creado un enumerado color con literales prametrizados Creado el atributo cadenaAmostrar Creado el constructor Color Creado el el metodo to String de la clase Color Creada la clase Posicón con sus atributos Creados los respectivos getters y setters de los Atributos Constructor creado Constructor copia creado Metodos equals u hasCode creados Metodo toString creado Creada la clase Dama con sus atributos Creados los getters y los setters para cada atributo Creado constructor por defecto constrtor con parametro color creado metodo crear crearPosicionInicial creado
    clase consola creada
    constructor Consola creado
    metodo elegir opciones creado

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
@@ -1,8 +1,17 @@
 package org.iesalandalus.programacion.damas.modelo;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
     private Consola(){
 
     }
+   public static void MostrarMenu(){
 
+       System.out.println("Menú de opciones, digite el numero de su opción");
+       System.out.println("1. Crear da dama dama por defecto");
+       System.out.println("2. Crear dama eligiendo color");
+       System.out.println("3. Mover dama");
+       System.out.println("4. Salir");
+   }
 }
