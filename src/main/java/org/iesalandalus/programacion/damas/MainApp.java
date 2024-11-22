bfa57bd05c04a74f7606a5a895202c6d51c379d4
barzo <barzomiadam@gmail.com>
2024-11-22 23:34:30 +0100

    Añadido mi nombre a Readme Creado el enumerado con literales parametrizados Creado el atributo String Constructor creado Metodo to String creado Creado un enumerado color con literales prametrizados Creado el atributo cadenaAmostrar Creado el constructor Color Creado el el metodo to String de la clase Color Creada la clase Posicón con sus atributos Creados los respectivos getters y setters de los Atributos Constructor creado Constructor copia creado Metodos equals u hasCode creados Metodo toString creado Creada la clase Dama con sus atributos Creados los getters y los setters para cada atributo Creado constructor por defecto constrtor con parametro color creado metodo crear crearPosicionInicial creado metodo mover creado metodo to String Creado clase consola creada constructor Consola creado metodo mostrar opciones creado metodo elegir opciones creado menú opciones color creado metodo mostrarMenúdirecciones metodo elegir direccion creada y metodo elegir color lo he modificado para que sea mas eficiente Metodo elegir pasos creado Metodo despedirse creado Atributo de clase dama creado en la clase MainApp Metodo ejecutarOpción creado

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -11,6 +11,28 @@ public class MainApp {
 
     private static Dama dama;
 
+    public static void ejecutarOpcion(int opcion) {//cremos un metodo para que llame a otros metodos con el switch
+        switch (opcion) {
+            case 1:
+                crearDamaDefecto();
+                break;
+            case 2:
+                crearDamaColor();
+                break;
+            case 3:
+                mover();
+                break;
+            case 4:
+                Consola.despedirse();
+                break;
+            default:
+                System.out.println("Opción no válida. Inténtelo de nuevo.");
+                break;
+        }
+    }
+}
+
+
 
 
 
